ace6de616077ee5ab96c471f6ef0d41fd1effb6b
Ripster47 <mpolinski12@gmail.com>
2019-01-17 12:02:48 -0600

    jethro adjust

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -17,7 +17,7 @@ class Api::GamesController < ApplicationController
     elsif @guess < answer
       @message_2 = "Too low!"
     elsif @guess > 100
-        @message_2 = "Woah there Jethro! Keep it between 1 and 100!"
+        @message_2 = "Hold on there Jethro! Keep it between 1 and 100!"
     else
       @message_2 = "Too high!"
     end

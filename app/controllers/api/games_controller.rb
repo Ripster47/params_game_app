938bc990ce279d32e477b857a844f688605cc3fd
Ripster47 <mpolinski12@gmail.com>
2019-01-17 10:52:00 -0600

    Jethro

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -16,10 +16,12 @@ class Api::GamesController < ApplicationController
       @message_2 = "You got it buddy!"
     elsif @guess < answer
       @message_2 = "Too low!"
+    elsif @guess > 100
+        @message_2 = "Woah there Jethro! Keep it between 1 and 100!"
     else
       @message_2 = "Too high!"
     end
-    
+
     render 'number_game.json.jbuilder'
   end
 
